86748d6de3dac4cacbca9901134cf5136e245ff5
Besser <125590422+Besser9009@users.noreply.github.com>
2024-04-11 13:51:01 +0300

    homework on the topic Lists and dictionaries

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ["apple", "banana", "coconat", "lichi", "mango", "orange"]
+print(my_list)
+print(my_list[0], " ", my_list[3])
+print(my_list[3:5])
+my_list[3] = "avocado"
+print(my_list)
+
+my_dict = {"Miolit":8794649654646, "Evvy":646166651664}
+print(my_dict["Miolit"])
+my_dict["Miolit"] = 616876749
+print(my_dict)
