0157939fc96f0394cb9107f4f5ccf233d78ef74e
Besser <125590422+Besser9009@users.noreply.github.com>
2024-04-12 13:15:46 +0300

    homework on the topic of the for loop. List items. Useful functions in the loop

---
+++ homework2_3.py
@@ -0,0 +1,10 @@
+car_brands = ["BMV", "MB", "LADA", "KIA", "HONDA"]
+for i in car_brands:
+    print("Я езжу на автомабиле марки", i)
+
+cars_count = 0
+for i in car_brands:
+    print("Я езжу на автомабиле марки", i)
+    cars_count += 10
+
+print(cars_count)
