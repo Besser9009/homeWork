16811bdfa28ba20ae949f9375731676226f6c5d6
Besser <125590422+Besser9009@users.noreply.github.com>
2024-04-13 13:11:55 +0300

    Update homework5.py
    refactor

--- homework5.py
+++ homework5.py
@@ -1,11 +1,12 @@
-my_list = ["apple", "banana", "coconat", "lichi", "mango", "orange"]
-print(my_list)
-print(my_list[0], " ", my_list[3])
-print(my_list[3:5])
-my_list[3] = "avocado"
+print(my_dict)my_list = ["apple", "banana", "coconat", "lichi", "mango", "orange"]
+print("List:", my_list)
+print("First element:", my_list[0])
+print("Last element:", my_list[-1])
+print(my_list[2:5])
+my_list[2] = "avocado"
 print(my_list)
 
-my_dict = {"Miolit":8794649654646, "Evvy":646166651664}
-print(my_dict["Miolit"])
-my_dict["Miolit"] = 616876749
+my_dict = {"Orange": "апельсин", "Kivi": "киви", "Banana": "кокос"}
+print(my_dict["Orange"])
+my_dict["Banana"] = "банан"
 print(my_dict)
