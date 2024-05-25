502c440d9b6826359212ce93e73998605256280b
Besser <125590422+Besser9009@users.noreply.github.com>
2024-05-25 20:27:22 +0300

    Update homework_Namespace_and_scopes.py

--- homework_Namespace_and_scopes.py
+++ homework_Namespace_and_scopes.py
@@ -3,5 +3,7 @@ def test_function():
         print('Я в области видимости функции test_function')
     inner_function()
 
+test_function()
+
 # inner_function()  Вызов не сработает так как она находится в локальном пространстве имён функции test_function()
 
